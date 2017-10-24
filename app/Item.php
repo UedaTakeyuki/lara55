7b2a9887fbb5e322cf66a4b82a5ae661690faaf8
Takeyuki UEDA <ueda@latelierdueda.com>
2017-10-24 12:28:23 +0900

    http://blog.asial.co.jp/1498

---
+++ app/Item.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    //
+		/**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
