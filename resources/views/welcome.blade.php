9db16268d8587ffe14d2d869e4c740369a49d022
Takeyuki UEDA <ueda@latelierdueda.com>
2017-10-24 10:30:50 +0900

    add vue esample

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,6 +89,10 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+                <div id="app">
+                    <example></example>
+                </div>
+                <script src="js/app.js"></script>
             </div>
         </div>
     </body>
